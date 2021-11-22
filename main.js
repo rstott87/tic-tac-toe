f3ae2070e8863b5e464351086cb1dbdfbc67af69
rstott87 <rstott87@yahoo.com>
2021-11-21 20:17:43 -0500

    added functionality to declare winner

--- main.js
+++ main.js
@@ -1,17 +1,105 @@
-const Gameboard = function (){
+
+const playerTurnMessage = document.getElementById("playerTurn");
+const boardContainer = document.querySelector("#gameboardContainer");
+const winnerDeclared = document.getElementById("winnerDeclared")
+const Gameboard = (function (){
     let board = []
+        
+    for (let i = 0; i<9; i++) {
+        const boardPiece = document.createElement('div');
+        boardContainer.appendChild(boardPiece)
+        boardPiece.classList.add('boardPiece')
+        boardPiece.setAttribute('id', i)
+        board.push(boardPiece);
+    ;}
+    return {board}
+})();
 
-        //let boardPiece = document.createElement('div')
-        //let boardContainer = document.getElementById("gameboardContainer");
-        //boardContainer.appendChild(boardPiece);
 
-};
+const winningArray1 = [];
+const winningArray2 = [];
+const winningArray3 = [];
+const winningArray4 = [];
+const winningArray5 = [];
+const winningArray6 = [];
+const winningArray7 = [];
+const winningArray8 = [];
 
-const boardPiece = document.querySelectorAll('.boardPiece')
-boardPiece.forEach((n) =>  {
+let winningArrays = [winningArray1, winningArray2, winningArray3, winningArray4, winningArray5, winningArray6, winningArray7, winningArray8]
+
+
+const renderedBoardPiece = document.querySelectorAll('.boardPiece');
+
+const createWinningArrays = (() => {
+    for (let i = 0; i < 9; i++) {
+        if (i <= 2){
+            winningArray1.push(renderedBoardPiece[i]);
+        }if (i > 2 && i <= 5) {
+            winningArray2.push(renderedBoardPiece[i]);
+        }if (i > 5 && i <= 8) {
+            winningArray3.push(renderedBoardPiece[i]);
+        }if (i == 0 || i == 3 || i == 6){
+            winningArray4.push(renderedBoardPiece[i]);
+        }if (i == 1 || i == 4 || i == 7) {
+            winningArray5.push(renderedBoardPiece[i]);
+        }if (i == 2 || i == 5 || i == 8) {
+            winningArray6.push(renderedBoardPiece[i]);
+        }if (i == 0 || i == 4 || i == 8) {
+            winningArray7.push(renderedBoardPiece[i]);
+        }if (i == 2 || i == 4 || i == 6) {
+            winningArray8.push(renderedBoardPiece[i]);
+        };
+    };
+})();
+
+
+let xTurn = true;
+
+
+renderedBoardPiece.forEach((n) =>  {
     n.addEventListener('click', () => {
-        console.log ("click")
-        n.innerHTML = 'X';
+        if (xTurn && n.innerHTML == ''){
+            n.innerHTML = 'X'
+            playerTurnMessage.innerHTML= "Player's Turn: O";
+            xTurn = false;
+            winningArrays.forEach(checkXForWinner);
+
+        } else if (!xTurn && n.innerHTML == '') {
+            n.innerHTML = 'O';
+            playerTurnMessage.innerHTML= "Player's Turn: X";
+            xTurn = true;
+            winningArrays.forEach(checkOForWinner);
+        }      
     }); 
-});
+}); 
+
+
+const checkXForWinner = (array) => {
+    for (let i = 0; i < 3; i ++) {
+        if (array[i].innerHTML == "X") {
+            }
+        else {
+            return 0
+            };
+        };   
+        playerTurnMessage.innerHTML= "X wins"
+        
+        array.forEach((n)=>{
+            n.style.backgroundColor = "grey"
+        })
+    };
+
+const checkOForWinner = (array) => {
+    for (let i = 0; i < 3; i ++) {
+        if (array[i].innerHTML == "O") {
+            }
+        else {
+            return 0
+            };
+        };   
+        playerTurnMessage.innerHTML= "O wins"
+        array.forEach((n)=>{
+            n.style.backgroundColor = "grey"
+        })    
+    };
 
